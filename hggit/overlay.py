3e56d8e6c8b110b67e4b07a7eba9609fc5c22723
Manuel Jacob <me@manueljacob.de>
2020-05-03 12:50:56 +0200

    py3: un-byteify argument name
    --HG--
    extra : amend_source : 24df563d9ba88fdb69618171ea10541a649a1297
    extra : histedit_source : 5b992dc39a3dc8d46a5bc3934383f93194b7c71c
    extra : rebase_source : 7e46780030c36d32d44e0deb5afee37176ffabc4
    extra : topic : byteify-sources

--- hggit/overlay.py
+++ hggit/overlay.py
@@ -163,7 +163,7 @@ def wrapmanifestdictdiff(orig, self, m2, match=None, clean=False):
         match = None
 
     kwargs = {
-        b'clean': clean
+        'clean': clean
     }
     # Older versions of mercurial don't support the match arg, so only add it
     # if it exists.
