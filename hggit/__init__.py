fd4e8cd20419a70b93b8c51fc09ddb549000c1a1
Georges Racinet <georges.racinet@octobus.net>
2020-05-13 14:41:19 +0200

    metadata: now minimum supported Mercurial version is 4.3
    Before Mercurial 4.3, the pycompat module doesn't have enough
    features to support our current dual Python code.
    
    Also, the oldest version currently available in Heptapod CI
    happens to be 4.3.
    
    It's also a good opportunity to start using the 'minimumhgversion'
    field, making the extension refuse to load on unsupported version.

--- hggit/__init__.py
+++ hggit/__init__.py
@@ -66,8 +66,9 @@ except AttributeError:  # pre 4.7 API
 
 __version__ = b'0.8.13'
 
-testedwith = (b'3.7.3 3.8.4 3.9.2 4.0.2 4.1.3 4.2.3 4.3.3 4.4.2 4.5.3 4.6.2 '
+testedwith = (b'4.3.3 4.4.2 4.5.3 4.6.2 '
               b'4.7.2 4.8.2 4.9.1 5.0.2 5.1.2 5.2')
+minimumhgversion = b'4.3'
 buglink = b'https://dev.heptapod.net/mercurial/hg-git/issues'
 
 cmdtable = {}
