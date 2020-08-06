92ab81ef8b42abe1e21875674a69547e744dd8b9
Georges Racinet <georges.racinet@octobus.net>
2020-08-06 16:29:12 +0200

    metadata: Mercurial 5.5 is supported
    We don't have a dedicated CI job for hg 5.5 right now, but the
    stable branch plays that role for now. Excerpts from a recent succesful
    pipeline:
    
      tests-py2-hg-stable:
        env HGRCPATH= python2 hg version
        Mercurial Distributed SCM (version 5.5+2-3d414dce2d40)
    
      tests-py3-hg-stable:
        env HGRCPATH= python3 hg version
        Mercurial Distributed SCM (version 5.5+2-3d414dce2d40)

--- hggit/__init__.py
+++ hggit/__init__.py
@@ -67,7 +67,7 @@ except AttributeError:  # pre 4.7 API
 __version__ = b'0.9.0a1'
 
 testedwith = (b'4.3.3 4.4.2 4.5.3 4.6.2 '
-              b'4.7.2 4.8.2 4.9.1 5.0.2 5.1.2 5.2 5.3 5.4')
+              b'4.7.2 4.8.2 4.9.1 5.0.2 5.1.2 5.2 5.3 5.4 5.5')
 minimumhgversion = b'4.3'
 buglink = b'https://foss.heptapod.net/mercurial/hg-git/issues'
 
