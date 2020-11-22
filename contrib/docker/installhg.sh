c1a9e768b2d6cb2a64948988f81c09b8dfe7c80c
Dan Villiom Podlaski Christiansen <danchr@gmail.com>
2020-11-22 03:24:32 +0100

    ci: simplify .gitlab-ci.yml, and generate own images
    --HG--
    extra : absorb_source : 9df525a790d1c79e87bcf5df0930c8b9a284dc12
    extra : amend_source : 0fe9e3b24291c1cb52759012aba513625e581179
    extra : histedit_source : 301223c321498ec4100fcf6b7a81e4c53d3699b0%2Cbe8a1bfd922fb1da7b7fd77087e86f6093787ac2
    extra : rebase_source : 0b07767029d64b4308c09b894583815030b02f59
    extra : topic : own-gitlab-ci

---
+++ contrib/docker/installhg.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+set -e
+
+BUILDDEPENDS="curl gcc gettext musl-dev"
+RUNDEPENDS="git unzip openssh gnupg"
+
+if echo "$HG" | fgrep -q .
+then
+    PIPDEPENDS="mercurial~=$HG.0"
+else
+    PIPDEPENDS="https://www.mercurial-scm.org/repo/hg/archive/$HG.tar.gz"
+fi
+
+if echo "$PYTHON" | grep -q ^2
+then
+    PIPDEPENDS="$PIPDEPENDS dulwich~=0.19.0"
+else
+    PIPDEPENDS="$PIPDEPENDS dulwich pyflakes"
+fi
+
+set -xe
+
+apk add $BUILDDEPENDS $RUNDEPENDS
+
+python -m pip --no-cache-dir install $PIPDEPENDS
+
+apk del $BUILDDEPENDS
