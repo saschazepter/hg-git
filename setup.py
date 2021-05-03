d47a75543439f7de0c2326c4bf2598db55bd5a9f
Dan Villiom Podlaski Christiansen <danchr@gmail.com>
2021-05-03 17:19:57 +0200

    setup: use setup.cfg & setuptools_scm

--- setup.py
+++ setup.py
@@ -1,42 +1,9 @@
-from __future__ import generator_stop
-
-from os.path import dirname, join
+import setuptools
 
 try:
-    from setuptools import setup
-except:
-    from distutils.core import setup
+    import setuptools_scm
+    assert setuptools_scm # silence pyflakes
+except ImportError:
+    pass
 
-
-def get_file(relpath):
-    root = dirname(__file__)
-    with open(join(root, relpath)) as fp:
-        return fp.read().strip()
-
-
-def get_version(relpath):
-    for line in get_file(relpath).splitlines():
-            line = line
-            if '__version__' in line:
-                return line.split("'")[1]
-
-
-setup(
-    name='hg-git',
-    version=get_version('hggit/__init__.py'),
-    author='The hg-git Authors',
-    maintainer='Kevin Bullock',
-    maintainer_email='kbullock+mercurial@ringworld.org',
-    url='http://foss.heptapod.net/mercurial/hg-git',
-    description='push to and pull from a Git repository using Mercurial',
-    long_description=get_file("README.rst"),
-    keywords='hg git mercurial',
-    license='GPLv2',
-    packages=['hggit'],
-    include_package_data=True,
-    zip_safe=True,
-    python_requires='>=3.6',
-    install_requires=[
-        'dulwich>=0.19.3',
-    ],
-)
+setuptools.setup()
