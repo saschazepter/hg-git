ac018e79515c4c5f4f07bc5196686802174dab32
Dan Villiom Podlaski Christiansen <danchr@gmail.com>
2021-05-05 10:59:18 +0200

    compat: add support for mercurial 5.8

--- hggit/git_handler.py
+++ hggit/git_handler.py
@@ -1860,7 +1860,7 @@ class GitHandler(object):
 
             # not available in dulwich 0.19, used on Python 2.7
             if hasattr(client, 'get_credentials_from_store'):
-                urlobj = hgutil.url(uri)
+                urlobj = compat.url(uri)
                 auth = client.get_credentials_from_store(
                     urlobj.scheme,
                     urlobj.host,
@@ -1894,7 +1894,7 @@ class GitHandler(object):
             )
 
         if uri.startswith(b'file://'):
-            return client.LocalGitClient(), hgutil.url(uri).path
+            return client.LocalGitClient(), compat.url(uri).path
 
         # if its not git or git+ssh, try a local url..
         return client.SubprocessGitClient(), uri
