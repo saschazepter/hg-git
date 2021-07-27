eacc5862d78be1aa78efaf4114eecd94e2db8225
Dan Villiom Podlaski Christiansen <danchr@gmail.com>
2021-07-27 10:22:57 +0200

    ci: allow using pre-releases
    pip install --pre mercurial~=5.9.0 doesn't work as expected, so we
    extract the versions using the PyPI API.

--- contrib/docker/installhg.sh
+++ contrib/docker/installhg.sh
@@ -2,15 +2,8 @@
 
 set -e
 
-BUILDDEPENDS="curl gcc gettext musl-dev"
+BUILDDEPENDS="curl jq coreutils gcc gettext musl-dev"
 RUNDEPENDS="git git-daemon unzip openssh gnupg"
-
-if echo "$HG" | fgrep -q .
-then
-    PIPDEPENDS="mercurial~=$HG.0"
-else
-    PIPDEPENDS="https://foss.heptapod.net/octobus/mercurial-devel/-/archive/branch/$HG.tar.gz"
-fi
 
 if echo "$PYTHON" | grep -q ^2
 then
@@ -29,5 +22,25 @@ set -xe
 apk add --no-cache $BUILDDEPENDS $RUNDEPENDS
 
 python -m pip --no-cache-dir install $PIPDEPENDS
+
+# handle pre-release versions
+get_version() {
+    curl -s "https://pypi.org/pypi/$1/json" \
+        | jq -r '.releases | keys_unsorted | .[]' \
+        | grep "^$2" \
+        | sort --version-sort \
+        | tail -1
+}
+
+hgversion=$(get_version mercurial $HG)
+
+if test -n "$hgversion"
+then
+    python -m pip install --pre mercurial==$hgversion
+else
+    # unreleased, so fetch directly from Heptapod itself
+    python -m pip install \
+        https://foss.heptapod.net/octobus/mercurial-devel/-/archive/branch/$HG/hg.tar.bz2
+fi
 
 apk del $BUILDDEPENDS
