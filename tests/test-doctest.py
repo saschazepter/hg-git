6b60b190ad3513b8bde2c6b6f98b6edd72c7c702
Dan Villiom Podlaski Christiansen <danchr@gmail.com>
2021-11-21 23:54:02 +0100

    tests: sync files from mercurial
    --HG--
    extra : histedit_source : 76b093e8eac7b29f803b68a5cc0815d19c2284bb
    extra : rebase_source : 70d56851bec1252b203d4f983e078264a3c8b76e
    extra : topic : coverage

--- tests/test-doctest.py
+++ tests/test-doctest.py
@@ -1,17 +1,22 @@
 # this is hack to make sure no escape characters are inserted into the output
 
-from __future__ import generator_stop
+from __future__ import absolute_import
+from __future__ import print_function
 
 import doctest
 import os
 import re
+import subprocess
 import sys
 
 # add hggit/ to sys.path
 sys.path.insert(0, os.path.join(os.environ["TESTDIR"], ".."))
 
+ispy3 = sys.version_info[0] >= 3
+
 if 'TERM' in os.environ:
     del os.environ['TERM']
+
 
 class py3docchecker(doctest.OutputChecker):
     def check_output(self, want, got, optionflags):
@@ -19,11 +24,18 @@ class py3docchecker(doctest.OutputChecker):
         got2 = re.sub(r'''\bb(['"])(.*?)\1''', r'\1\2\1', got)  # py3: b''
         # py3: <exc.name>: b'<msg>' -> <name>: <msg>
         #      <exc.name>: <others> -> <name>: <others>
-        got2 = re.sub(r'''^hggit\.\w+\.(\w+): (['"])(.*?)\2''', r'\1: \3',
-                      got2, re.MULTILINE)
-        got2 = re.sub(r'^hggit\.\w+\.(\w+): ', r'\1: ', got2, re.MULTILINE)
-        return any(doctest.OutputChecker.check_output(self, w, g, optionflags)
-                   for w, g in [(want, got), (want2, got2)])
+        got2 = re.sub(
+            r'''^mercurial\.\w+\.(\w+): (['"])(.*?)\2''',
+            r'\1: \3',
+            got2,
+            re.MULTILINE,
+        )
+        got2 = re.sub(r'^mercurial\.\w+\.(\w+): ', r'\1: ', got2, re.MULTILINE)
+        return any(
+            doctest.OutputChecker.check_output(self, w, g, optionflags)
+            for w, g in [(want, got), (want2, got2)]
+        )
+
 
 def testmod(name, optionflags=0, testtarget=None):
     __import__(name)
@@ -33,13 +45,90 @@ def testmod(name, optionflags=0, testtarget=None):
 
     # minimal copy of doctest.testmod()
     finder = doctest.DocTestFinder()
-    checker = py3docchecker()
+    checker = None
+    if ispy3:
+        checker = py3docchecker()
     runner = doctest.DocTestRunner(checker=checker, optionflags=optionflags)
     for test in finder.find(mod, name):
         runner.run(test)
     runner.summarize()
 
-testmod('hggit.compat')
-testmod('hggit.hg2git')
-testmod('hggit.util')
-testmod('hggit.git_handler')
+
+DONT_RUN = []
+
+# Exceptions to the defaults for a given detected module. The value for each
+# module name is a list of dicts that specify the kwargs to pass to testmod.
+# testmod is called once per item in the list, so an empty list will cause the
+# module to not be tested.
+testmod_arg_overrides = {}
+
+fileset = 'set:(**.py)'
+
+cwd = os.path.dirname(os.environ["TESTDIR"])
+
+if not os.path.isdir(os.path.join(cwd, ".hg")):
+    sys.exit(0)
+
+files = subprocess.check_output(
+    "hg files --print0 \"%s\"" % fileset,
+    shell=True,
+    cwd=cwd,
+    stderr=subprocess.DEVNULL,
+).split(b'\0')
+
+if sys.version_info[0] >= 3:
+    cwd = os.fsencode(cwd)
+
+mods_tested = set()
+for f in files:
+    if not f:
+        continue
+
+    with open(os.path.join(cwd, f), "rb") as fh:
+        if not re.search(br'\n\s*>>>', fh.read()):
+            continue
+
+    if ispy3:
+        f = f.decode()
+
+    modname = f.replace('.py', '').replace('\\', '.').replace('/', '.')
+
+    # Third-party modules aren't our responsibility to test, and the modules in
+    # contrib generally do not have doctests in a good state, plus they're hard
+    # to import if this test is running with py2, so we just skip both for now.
+    if modname.startswith('mercurial.thirdparty.') or modname.startswith(
+        'contrib.'
+    ):
+        continue
+
+    for kwargs in testmod_arg_overrides.get(modname, [{}]):
+        mods_tested.add((modname, '%r' % (kwargs,)))
+        if modname.startswith('tests.'):
+            # On py2, we can't import from tests.foo, but it works on both py2
+            # and py3 with the way that PYTHONPATH is setup to import without
+            # the 'tests.' prefix, so we do that.
+            modname = modname[len('tests.') :]
+
+        testmod(modname, **kwargs)
+
+# Meta-test: let's make sure that we actually ran what we expected to, above.
+# Each item in the set is a 2-tuple of module name and stringified kwargs passed
+# to testmod.
+expected_mods_tested = set(
+    [
+        ('hggit.git_handler', '{}'),
+        ('hggit.util', '{}'),
+    ]
+)
+
+unexpectedly_run = mods_tested.difference(expected_mods_tested)
+not_run = expected_mods_tested.difference(mods_tested)
+
+if unexpectedly_run:
+    print('Unexpectedly ran (probably need to add to list):')
+    for r in sorted(unexpectedly_run):
+        print('  %r' % (r,))
+if not_run:
+    print('Expected to run, but was not run (doctest removed?):')
+    for r in sorted(not_run):
+        print('  %r' % (r,))
