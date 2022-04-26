55a5ea9654c30adca51e89c467fac2f6ce1ffc93
Dan Villiom Podlaski Christiansen <danchr@gmail.com>
2022-04-26 17:12:17 +0200

    core: transfer git commit map and tags between hg repos
    This implements transferring the Git state between Mercurial
    repositories, and saving it in bundles. The behaviour is disabled by
    default, hidden behind two experimental options.
    
    We do so by embedding optional bundle2 parts that contain tags and git
    mapping for the changesets in the bundle2. For local clones, we add
    the mapfile and tags file to list of files to copy.
    
    There is no support for reinstating or synchronising lost or old
    state, and no support for transferring tags to old changesets. The
    main upside to this is that we've restricted the potential for
    conflicts massively.
    
    Since this makes it more likely that users will run into the case
    where a git parent is missing, I've tweaked the error message.
    
    --HG--
    extra : rebase_source : 1c5cc983af9127b58fd693da432c5c618ef207c5
    extra : topic : transfer-git-state

--- hggit/hgrepo.py
+++ hggit/hgrepo.py
@@ -1,15 +1,25 @@
 from __future__ import generator_stop
 
+import io
+
+from mercurial import bundle2
+from mercurial import exchange
 from mercurial import exthelper
 from mercurial import repoview
 from mercurial import util as hgutil
-from mercurial.node import bin
+from mercurial.node import bin, hex
 
 from .git_handler import GitHandler
 from .gitrepo import gitrepo
 from . import util
 
 eh = exthelper.exthelper()
+
+CAPABILITY_MAP = b'exp-hg-git-map'
+CAPABILITY_TAGS = b'exp-hg-git-tags'
+
+BUNDLEPART_MAP = b'exp-hg-git-map'
+BUNDLEPART_TAGS = b'exp-hg-git-tags'
 
 
 @eh.reposetup
@@ -85,3 +95,121 @@ def pinnedrevs(orig, repo):
         pinned.update(rev(r) for r in repo.githandler.remote_refs.values())
 
     return pinned
+
+
+@eh.wrapfunction(bundle2, 'getrepocaps')
+def getrepocaps(orig, repo, **kwargs):
+    caps = orig(repo, **kwargs)
+
+    if repo.ui.configbool(b'experimental', b'hg-git-serve'):
+        caps[CAPABILITY_MAP] = ()
+        caps[CAPABILITY_TAGS] = ()
+
+    return caps
+
+
+def addpartrevgitmap(repo, bundler, outgoing):
+    if repo.githandler:
+        # this is different from what we store in the repository,
+        # and uses binary node ids: <20 bytes> <20 bytes>
+        repo.ui.debug(b'bundling git map\n')
+
+        chunks = (
+            bin(repo.githandler._map_hg[hex(hgnode)]) + hgnode
+            for hgnode in outgoing.missing
+            if hex(hgnode) in repo.githandler._map_hg
+        )
+
+        bundler.newpart(BUNDLEPART_MAP, data=chunks, mandatory=False)
+
+
+def addpartrevgittags(repo, bundler, outgoing):
+    if repo.githandler.tags:
+        # this is consistent with the format used in the repository:
+        # <40 hex digits> <space> <tag name> <newline>
+        repo.ui.debug(b'bundling git tags\n')
+
+        chunks = (
+            b"%s %s\n" % (sha, name)
+            for name, sha in sorted(repo.githandler.tags.items())
+            if bin(sha) in outgoing.missing
+        )
+
+        bundler.newpart(BUNDLEPART_TAGS, data=chunks, mandatory=False)
+
+
+@eh.wrapfunction(bundle2, '_addpartsfromopts')
+def _addpartsfromopts(orig, ui, repo, bundler, source, outgoing, opts):
+    orig(ui, repo, bundler, source, outgoing, opts)
+
+    if opts.get(b'exp-hg-git', False) or ui.configbool(
+        b'experimental', b'hg-git-bundle'
+    ):
+        addpartrevgitmap(repo, bundler, outgoing)
+        addpartrevgittags(repo, bundler, outgoing)
+
+
+@eh.extsetup
+def install_server_support(ui):
+    @bundle2.parthandler(BUNDLEPART_MAP)
+    def handlebundlemap(op, inpart):
+        ui.debug(b'unbundling git map\n')
+
+        while True:
+            # this is different from what we store in the repository,
+            # and uses binary node ids: <20 bytes> <20 bytes>
+            line = inpart.read(40)
+
+            if inpart.consumed:
+                break
+
+            gitsha = hex(line[:20])
+            hgsha = hex(line[20:])
+
+            op.repo.githandler.map_set(gitsha, hgsha)
+
+        op.repo.githandler.save_map()
+
+    @exchange.getbundle2partsgenerator(BUNDLEPART_MAP)
+    def gitmapbundle(
+        bundler, repo, source, bundlecaps=None, b2caps=None, **kwargs
+    ):
+        if not b2caps or CAPABILITY_MAP not in b2caps:
+            return
+
+        ui.debug(b'bundling git map\n')
+
+        outgoing = exchange._computeoutgoing(
+            repo,
+            kwargs['heads'],
+            kwargs['common'],
+        )
+
+        addpartrevgitmap(repo, bundler, outgoing)
+
+    @bundle2.parthandler(BUNDLEPART_TAGS)
+    def handlebundletags(op, inpart):
+        with io.BytesIO() as buf:
+            while not inpart.consumed:
+                buf.write(inpart.read())
+            buf.seek(0)
+
+            # we're consistent, and always load everything, so just
+            # let the handler do its thing
+            op.repo.githandler._read_tags_from(buf)
+            op.repo.githandler.save_tags()
+
+    @exchange.getbundle2partsgenerator(BUNDLEPART_TAGS)
+    def gittagbundle(
+        bundler, repo, source, bundlecaps=None, b2caps=None, **kwargs
+    ):
+        if not b2caps or CAPABILITY_TAGS not in b2caps:
+            return
+
+        outgoing = exchange._computeoutgoing(
+            repo,
+            kwargs['heads'],
+            kwargs['common'],
+        )
+
+        addpartrevgittags(repo, bundler, outgoing)
--- tests/test-serve-hg.t
+++ tests/test-serve-hg.t
@@ -1,17 +1,39 @@
 #require serve
 
-  $ hg init test
-  $ cd test
+#testcases with-hggit without-hggit
+
+Load commonly used test logic
+  $ . "$TESTDIR/testutil"
+
+#if with-hggit
+  $ cat >> $HGRCPATH <<EOF
+  > [experimental]
+  > hg-git-serve = yes
+  > EOF
+#endif
+
+  $ git init gitrepo
+  Initialized empty Git repository in $TESTTMP/gitrepo/.git/
+  $ cd gitrepo
   $ echo foo>foo
   $ mkdir foo.d foo.d/bAr.hg.d foo.d/baR.d.hg
+  $ git add .
+  $ fn_git_commit -m 1
+  $ git tag thetag
   $ echo foo>foo.d/foo
   $ echo bar>foo.d/bAr.hg.d/BaR
   $ echo bar>foo.d/baR.d.hg/bAR
-  $ hg commit -A -m 1
-  adding foo
-  adding foo.d/bAr.hg.d/BaR
-  adding foo.d/baR.d.hg/bAR
-  adding foo.d/foo
+  $ git add .
+  $ fn_git_commit -m 2
+  $ cd ..
+
+  $ hg clone gitrepo hgrepo
+  importing 2 git commits
+  new changesets f488b65fa424:c61c38c3d614 (2 drafts)
+  updating to branch default (no-hg57 !)
+  updating to bookmark master (hg57 !)
+  4 files updated, 0 files merged, 0 files removed, 0 files unresolved
+  $ cd hgrepo
   $ hg serve -p $HGPORT -d --pid-file=../hg1.pid -E ../error.log
   $ hg --config server.uncompressed=False serve -p $HGPORT1 -d --pid-file=../hg2.pid
 
@@ -29,17 +51,45 @@ Test server address cannot be reused
   $ cd ..
   $ cat hg1.pid hg2.pid >> $DAEMON_PIDS
 
-Load commonly used test logic
-  $ . "$TESTDIR/testutil"
-
-Make sure that clone regular mercurial repos over http doesn't break
+Make sure that clone regular mercurial repos over http doesn't break,
+and that we can transfer the hg-git metadata
 
   $ hg clone http://localhost:$HGPORT/ copy 2>&1
   requesting all changes
   adding changesets
   adding manifests
   adding file changes
-  added 1 changesets with 4 changes to 4 files
-  new changesets 8b6053c928fe (?)
+  added 2 changesets with 4 changes to 4 files
+  new changesets f488b65fa424:c61c38c3d614 (?)
   updating to branch default
   4 files updated, 0 files merged, 0 files removed, 0 files unresolved
+  $ cd copy
+#if without-hggit
+  $ hg tags
+  tip                                1:c61c38c3d614
+  $ hg log -T '{rev}:{node|short} | {bookmarks} | {gitnode} |\n'
+  1:c61c38c3d614 | master |  |
+  0:f488b65fa424 |  |  |
+  $ hg pull -u ../gitrepo
+  pulling from ../gitrepo
+  importing 2 git commits
+  0 files updated, 0 files merged, 0 files removed, 0 files unresolved
+#else
+  $ hg tags
+  tip                                1:c61c38c3d614
+  thetag                             0:f488b65fa424
+  $ hg log -T '{rev}:{node|short} | {bookmarks} | {gitnode} |\n'
+  1:c61c38c3d614 | master | 95bcbb72932335c132c10950b5e5dc1066138ea1 |
+  0:f488b65fa424 |  | a874aa4c9506ed30ef2c2c7313abd2c518e9e71e |
+  $ hg pull -u ../gitrepo
+  pulling from ../gitrepo
+  warning: created new git repository at $TESTTMP/copy/.hg/git
+  no changes found
+#endif
+
+  $ hg tags
+  tip                                1:c61c38c3d614
+  thetag                             0:f488b65fa424
+  $ hg log -T '{rev}:{node|short} | {bookmarks} | {gitnode} |\n'
+  1:c61c38c3d614 | master | 95bcbb72932335c132c10950b5e5dc1066138ea1 |
+  0:f488b65fa424 |  | a874aa4c9506ed30ef2c2c7313abd2c518e9e71e |
