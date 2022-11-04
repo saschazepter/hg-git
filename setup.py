a4282e223b241f8ae1ac6f51faf01d8a26d0d6cf
Dan Villiom Podlaski Christiansen <danchr@gmail.com>
2022-11-04 18:16:00 +0100

    setup: don't handle missing setuptools_scm gracefully
    There's little point in building or installing hg-git, other than to
    get the appropriate version markers and dependencies. `python
    ./setup.py build` doesn't do that, and a straight install that way can
    lead to a broken setup. Instead, treat it as an hard error.
    
    https://foss.heptapod.net/mercurial/hg-git/-/issues/389

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 import setuptools
 
-try:
-    import setuptools_scm
+# don't guard this with a try; users should use pip for
+# installing/building hg-git, as it ensures the proper dependencies
+# are present
+import setuptools_scm
 
-    assert setuptools_scm  # silence pyflakes
-except ImportError:
-    pass
+assert setuptools_scm  # silence pyflakes
 
 setuptools.setup()
