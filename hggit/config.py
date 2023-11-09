a2db2fa9bc6da3e35434efd631d63d5635db1d7f
Dan Villiom Podlaski Christiansen <danchr@gmail.com>
2023-11-09 03:26:43 +0100

    core: remove debugextrainmessage support
    The stated purpose for this is for testing. However, it's not for
    testing of hg-git, but of git-remote-hg. The mode has been broken for
    years, though, and given that it doesn't actually rely on the
    known-good modes of hg-git, it doesn't offer much guarantee of
    anything actually working.
    
    Removing as general cruft that needlessly complicates the codebase.
    
    Closes: #281 !211

--- hggit/config.py
+++ hggit/config.py
@@ -18,7 +18,6 @@ CONFIG_DEFAULTS = {
     b'git': {
         b'authors': None,
         b'branch_bookmark_suffix': None,
-        b'debugextrainmessage': False,  # test only -- do not document this!
         b'findcopiesharder': False,
         b'intree': None,
         b'mindate': None,
