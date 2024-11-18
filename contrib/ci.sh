44b01ea7dc5481afd427a80e533bf13bb2675e73
Dan Villiom Podlaski Christiansen <danchr@gmail.com>
2024-11-18 23:21:36 +0100

    tests: always write coverage to `outputdir`
    --HG--
    extra : amend_source : f948f2162c5be8699a8a135b52ff66b1526d5e90
    extra : rebase_source : c1076e725c03685ad6259e33c8d2e5dac0c5dd44
    extra : topic : sync-runtests

--- contrib/ci.sh
+++ contrib/ci.sh
@@ -6,7 +6,10 @@ git version
 hg debuginstall --config extensions.hggit=./hggit
 hg version -v --config extensions.hggit=./hggit
 
+mkdir -p out
+
 exec python$PYTHON tests/run-tests.py \
      --color=always \
+     --outputdir out \
      --cover \
-     --xunit $PWD/tests-$CI_JOB_ID.xml
+     --xunit tests-$CI_JOB_ID.xml
