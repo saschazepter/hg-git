162854228971b3dfa65a97180073ad5d6a2bc56b
muxator <a.mux@inwind.it>
2025-11-24 18:00:23 +0100

    setup: in case of subprocess error in setuptools.setup(), print std{out,err}
    This problem caused a hard to debug CI failure for ~9 months: setuptools.setup()
    would call an "hg log" command, that would fail for a permission problem. The
    information was not stored in the exception message, but in the captured stderr,
    that was never printed.
    
    A failure is now uglier, but more explicit:
    ```
    + exec python3.13 tests/run-tests.py '--color=always' --cover --xunit /builds/mercurial/hg-git/tests-3582275.xml
    A CalledProcessError was raised:
    Command '['hg', 'log', '-r', '.', '-T', '{node}\n{tag}\n{date|shortdate}']' returned non-zero exit status 255.
    STDOUT:
    =============
    STDERR:
    not trusting file /builds/mercurial/hg-git/.hg/hgrc from untrusted user root, group root
    not trusting file /builds/mercurial/hg-git/.hg/hgrc from untrusted user root, group root
    abort: Permission denied: '/builds/mercurial/hg-git/.hg/dirstate'  <-- THIS WAS THE MISSING INFORMATION
    =============
    Traceback (most recent call last):
    [...]
    subprocess.CalledProcessError: Command '['hg', 'log', '-r', '.', '-T', '{node}\n{tag}\n{date|shortdate}']' returned non-zero exit status 255.
    ```
    
    --HG--
    branch : 1.2.x
    extra : histedit_source : b2a02de23151f60759544f25f7effab27f8169d2
    extra : topic : debug-ci

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 import setuptools
+import subprocess
+import sys
 
 # don't guard this with a try; users should use pip for
 # installing/building hg-git, as it ensures the proper dependencies
@@ -7,4 +9,11 @@ import setuptools_scm
 
 assert setuptools_scm  # silence pyflakes
 
-setuptools.setup()
+try:
+    setuptools.setup()
+except subprocess.CalledProcessError as e:
+    print("A CalledProcessError was raised:", file=sys.stderr)
+    print(e, file=sys.stderr)
+    print(f"STDOUT:\n{e.stdout}\n=============\n", file=sys.stderr)
+    print(f"STDERR:\n{e.stderr}\n=============\n", file=sys.stderr)
+    raise
