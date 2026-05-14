798a618e5ea4d919cefbddfb6a0f1d2518ae50f5
muxator <a.mux@inwind.it>
2026-05-15 00:50:23 +0200

    ci: fix the CI by pinning "coverage" version to 7.12.0
    We have unpinned dependencies all over the place in the project, which means
    that things are prone to break as a simple consequence of time passing, and not
    of any actions on our part.
    
    In this case, "coverage" 7.13.0 started complaining that there was no .coveragerc
    file in <BASE>/tests.
    
    Putting an empty file in <BASE>/tests/.coveragerc would fix the issue for now,
    but not having a pinned dependency is a time bomb anyway. Moreover, the position
    of the file would be accidentally dependent on cwd, and not controlled by us.
    
    --HG--
    extra : _rewrite_noise : c8be123875e7b6fe2a98547060f68f11bbc8fc64
    extra : topic : pin-coverage-and-fix-ci

--- contrib/docker/installhg.sh
+++ contrib/docker/installhg.sh
@@ -4,7 +4,30 @@ set -e
 
 BUILDDEPENDS="curl jq coreutils gcc gettext musl-dev"
 RUNDEPENDS="git git-daemon unzip openssh gnupg"
-PIPDEPENDS="black coverage dulwich pyflakes pygments pylint setuptools_scm"
+
+# coverage >= 7.13.0 would break with:
+#
+# Traceback (most recent call last):
+#   File "/usr/lib64/python3.10/site.py", line 195, in addpackage
+#     exec(line)
+#   File "<string>", line 1, in <module>
+#   File "<string>", line 15, in <module>
+#   File "<PYTHONPATH>/site-packages/coverage/control.py", line 1491, in process_startup
+#     cov = Coverage(config_file=config_file)
+#   File "<PYTHONPATH>/site-packages/coverage/control.py", line 325, in __init__
+#     self.config = read_coverage_config(
+#   File "<PYTHONPATH>/site-packages/coverage/config.py", line 702, in read_coverage_config
+#     raise ConfigError(f"Couldn't read {fname!r} as a config file")
+# coverage.exceptions.ConfigError: Couldn't read '<BASE>/tests/.coveragerc' as a config file
+#
+# A simple fix would be creating an empty file in <BASE>/tests/.coveragerc, but
+# it would be fragile: the file position would depend on the current working
+# directory. Using a fixed absolute path would be better, but coverage is
+# controlled by run-tests.py, which is a very complex script.
+#
+# Fixing coverage version to 7.12.0, which is the latest one that works without
+# requiring less controllable changes.
+PIPDEPENDS="black coverage==7.12.0 dulwich pyflakes pygments pylint setuptools_scm"
 
 PIP="python -m pip --no-cache-dir"
 
