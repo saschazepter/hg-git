7d15caa48c67d47021c0d67a554ecd6bc36785d5
Antonio Muci <a.mux@inwind.it>
2026-06-18 22:03:18 +0200

    contrib: backed out changeset b08668744e06
    Let's verify if I was dreaming or if it really had an impact.
    
    --HG--
    extra : amend_source : 79cba22814a244be4b72d94765813801d41b6c6b
    extra : topic : fix_ci_dependencies

--- contrib/ci.sh
+++ contrib/ci.sh
@@ -2,24 +2,6 @@
 
 set -x
 
-# MUXATOR 2026-06-18
-#
-# CI tests were failing with:
-#    AssertionError: Invalid sideband channel 3
-#
-# see: https://foss.heptapod.net/mercurial/hg-git/-/jobs/4221633
-#
-# Looking up this error for hg-git, pops up this thread from 2016:
-#     https://github.com/schacon/hg-git/issues/289
-#
-# A solution suggested at the time was running "easy_install keyring".
-#
-# A possible alternative, that worked in the past when I had a similar problem
-# with poetry, is to set PYTHON_KEYRING_BACKEND.
-#
-# Let's test it and see what happens.
-export PYTHON_KEYRING_BACKEND=keyring.backends.fail.Keyring
-
 git version
 hg debuginstall --config extensions.hggit=./hggit
 hg version -v --config extensions.hggit=./hggit
